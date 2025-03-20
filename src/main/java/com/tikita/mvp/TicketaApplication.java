37b8ac7b4442a06e9b2c83c5a34cc4f0b41135a6
sean <siyeon0805@kakao.com>
2025-03-20 23:17:03 +0900

    프로젝트 생성

---
+++ src/main/java/com/tikita/mvp/TicketaApplication.java
@@ -0,0 +1,13 @@
+package com.tikita.mvp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TicketaApplication.class, args);
+    }
+
+}
